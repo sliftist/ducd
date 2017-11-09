f7bcd8b60324196a765148a4de625b5b61fa8fd5
Michael Glatzhofer <Michael.Glatzhofer@student.tugraz.at>
2017-11-09 22:47:01 +0100

    sub fix

--- src/util.ts
+++ src/util.ts
@@ -2,8 +2,8 @@
 export function tosub(s)
 {
     var result = ''
-    var str = s.toString()
-    for (var i = 0; i < str.length-1; i++)
+    var str = s.toString().trim()
+    for (var i = 0; i < str.length; i++)
         result += String.fromCharCode(8320 + Number(str.charAt(i)))
     return result
 }
